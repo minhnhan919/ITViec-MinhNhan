3256189a02b42aaa5492f074e7c60762bfc429b2
Minh Nhân <tlhoangminhnhan2004@gmail.com>
2025-03-04 14:52:15 +0700

    Updates

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', (event) => {
+  console.log('✅ Service Worker đã cài đặt');
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
